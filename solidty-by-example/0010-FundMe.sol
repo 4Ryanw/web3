9e0789aa781571662ba07d3ad7a289483bd7d29f
4Ryanw <enxi4r@163.com>
2025-01-24 17:19:02 +0800

    预言机设置转账最小值

--- solidty-by-example/0010-FundMe.sol
+++ solidty-by-example/0010-FundMe.sol
@@ -6,7 +6,7 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 contract FundMe{
 
     mapping(address=>uint256) public funderToAmount;
-    uint256 MINIMUM_VALUE = 100*10**18;
+    uint256 MINIMUM_VALUE = 50*(10**18); //最小值100美元
     AggregatorV3Interface internal dataFeed;
     //构造函数 
     constructor(){
@@ -17,7 +17,7 @@ contract FundMe{
     
     function fund() external  payable {
         //校验转账金额
-        require(convertETH2USD(msg.value) >= MINIMUM_VALUE,"send me more");
+       require(convertETH2USD(msg.value) >= MINIMUM_VALUE,"send me more!!");
         //记录转账人
         funderToAmount[msg.sender] = msg.value;
 
@@ -36,10 +36,13 @@ contract FundMe{
         return answer;
     }
 
-    function convertETH2USD(uint256 ethAmount) internal view returns(uint256){
-        //这里的price指的是一个ETH值多少个USD
+    // 将转账金额计算转化为usd
+    //1 ETH = 1000 FINNEY = 10**9 GWEI = 10**18WEI
+    // 入参N个wei 返回的是价值x美元
+    function   convertETH2USD(uint256 ethAmount) public view returns(uint256){
+        //这里的price指的是一个ETH值多少个USD ,有精确度precision ETH对USD 的精确度为10**8  ,换句话说这里chainLink预言机返回的price单位是 10^8*$/ETH 
         uint256 ethPrice =  uint256(getChainlinkDataFeedLatestAnswer()) ;
-        return (ethAmount/(10 ** 8)) * ethPrice;
+        return ethAmount * ethPrice/(10 ** 8);
     }
 
 }
--- solidty-by-example/005-Constant.sol
+++ solidty-by-example/005-Constant.sol
@@ -6,6 +6,7 @@ contract Constants{
     address public constant MY_ADDRESS = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
     uint public constant MY_UINT  = 123;
 
-    function test() public {
+    function test() public payable returns(uint256){
+            return msg.value;
     }
 }
