b8a0ffa0d118853f8dfd0ec333e88122ded6e1c3
4Ryanw <enxi4r@163.com>
2025-01-25 21:31:17 +0800

    web3

--- solidty-by-example/002-Function.sol
+++ solidty-by-example/002-Function.sol
@@ -1,18 +1,28 @@
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
-contract Counter{
-    //uint初始默认值是0
-    uint  count = 3;
+contract HelloWorld{
+string  str = 'Hello World!';
+/**
+* 1.关键字function,声明这是一个函数
+* 2.函数名
+* 3.入参 
+* 4.权限修饰符 见4.1
+* 5.函数功能关键字（view、pure等）
+* 6.若有返回值，关键字returns+返回数据类型
+* 7.存储模式关键字 见4.2
+*/
+  //无入参 有返回值的函数 view - 纯读取函数
+  function sayHello() public view returns(string memory){
+			return   addinfo(str);
+		}
+   //有入参 无返回值的函数 修改变量的函数
+   function setHelloWorld(string memory newString) public{
+   	str = newString;
+   }
+    //有入参和返回值 纯函数（只有计算功能，不读取或修改变量）
+   function addinfo(string memory newString) internal pure returns(string memory){
 
-    function get() public view returns (uint){
-        return count ;
-    }
-
-    function incr() public {
-        count += 1;
-    }
-      function decr() public {
-        count -= 1;
-    }
+   return string.concat(newString,"test");
+   }
 }
---
+++ solidty-by-example/010-FundMe.sol
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+//1、创建一个收款函数可以收款 
+//2、记录投资人并查看
+//3、在锁定期内，当筹款达到目标值生产商可以提款
+//4、在锁定期内，未达到目标值投资人可以退款
+contract FundMe{
+
+    mapping(address=>uint256) public funderToAmount;
+    uint256 MINIMUM_VALUE = 50*(10**18); //最小值100美元
+    AggregatorV3Interface internal dataFeed;
+    //定义目标
+    uint256 constant TARGET = 1000*10**18;
+    address public owner ;
+    //unix时间戳标准
+    uint256 deploymentTimestamp;
+    uint256 lockTime;
+    //构造函数 
+    constructor(uint256 _lockTime){
+        //sepolia 测试网
+        dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        owner = msg.sender;
+        //当前区块的时间点
+        deploymentTimestamp = block.timestamp;
+        //设置锁定时间
+        lockTime = _lockTime;
+    }
+    
+        //payable关键字 表示该函数可以收取链上的原生通证
+    //1.收款函数
+    //只能在锁定期内调用
+    function fund() external  payable {
+        require(block.timestamp<=deploymentTimestamp+lockTime,"window is closed");
+         //msg全局变量 sender=投送人 ， value=投送数量
+        //校验转账金额
+       require(convertETH2USD(msg.value) >= MINIMUM_VALUE,"send me more!!");
+        //记录转账人
+        funderToAmount[msg.sender] = msg.value;
+
+    }
+
+
+  function getChainlinkDataFeedLatestAnswer() public view returns (int) {
+        // prettier-ignore
+        (
+            /* uint80 roundID */,
+            int answer,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = dataFeed.latestRoundData();
+        return answer;
+    }
+
+    // 将转账金额计算转化为usd
+    //1 ETH = 1000 FINNEY = 10**9 GWEI = 10**18WEI
+    // 入参N个wei 返回的是价值 m 单位是10**18美元
+    function   convertETH2USD(uint256 ethAmount) public view returns(uint256){
+        //这里的price指的是一个ETH值多少个USD ,有精确度precision ETH对USD 的精确度为10**8  ,换句话说这里chainLink预言机返回的price单位是 10^8*$/ETH 
+        uint256 ethPrice =  uint256(getChainlinkDataFeedLatestAnswer()) ;
+        return ethAmount * ethPrice/(10 ** 8);
+    }
+
+    function transferOwnership(address newOwner) public ownerOnly{
+        owner = newOwner;
+    }
+
+        //取款
+    function getFund()  external windowClose ownerOnly{
+        //this关键字 表示当前合约
+        //balance 单位是wei 需要转换成美元
+        require(convertETH2USD(address(this).balance) >= TARGET,"Targer is not reached");
+        //transfer：transfer ETH revert if tx failed 纯转账
+        payable (msg.sender).transfer(address(this).balance);
+        // send 纯转账 会返回执行结果 布尔类型
+         // eg: bool success =  payable (msg.sender).send(address(this).balance);
+        // call 转账过程中调用函数 ，兼容transfer和send， 官方建议都用call
+        // 若调用的函数有返回值则可以将返回值一并返回，并且会返回一个交易成功与否的bool
+        bool  success;
+          (success,) = payable (msg.sender).call{value: address(this).balance}("");
+
+    }
+
+    function refund() external windowClose{
+        //目标值检测
+        require(convertETH2USD(address(this).balance)< TARGET,'Target is reached' );
+        uint256 amount =  funderToAmount[msg.sender];
+        //投资金额检查
+        require(amount != 0,"there is no found for you ");
+          bool  success;
+         (success,) = payable (msg.sender).call{value: amount}("");
+         require(success,"transfer is failed!");
+         funderToAmount[msg.sender] = 0;
+
+    }
+    modifier windowClose(){
+        require(block.timestamp>=deploymentTimestamp+lockTime,"window is not closed");
+        _; //下划线的位置表明后续代码执行的顺序和require的优先级
+
+    }
+
+    modifier ownerOnly(){
+        require(msg.sender == owner,"this function can only be called by owner");
+        _;
+    }
+
+}
